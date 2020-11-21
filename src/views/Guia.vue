148e0004dfb22b1d5c0f443ec93486b8569c7717
Carlos Salinas <carlos.ariel.salinas@hotmail.com>
2020-11-21 13:29:20 -0300

    arreglo en guia

--- src/views/Guia.vue
+++ src/views/Guia.vue
@@ -6,9 +6,9 @@
       <ul class="text-justify mt-2" style="width:95%;">
         <li>Las tareas se agregan mediante el campo principal. Al presionar Enter la tarea se agregará al listado.</li>
         <li>Cada tarea tiene la opcíon de marcarla como 'Completada'.</li>
-        <li>Cuando una tarea se marca como completada, la misma aparecerá como tachada.</li>
-        <li>Para editar una tarea, haga doble click sobre la tarea que desea editar. Una vez que termine de editar, presione Enter para guardar los cambios.</li>
-        <li>Para eliminar una tarea, haga click sobre el icono 'X'. En el caso de que la tarea no este completa, le solicitará una confirmación para poder eliminarla.</li>
+        <li>Cuando una tarea se marca como completada, la misma aparecerá tachada.</li>
+        <li>Para editar una tarea, haga doble click sobre la que desee editar. Una vez que termine de editar, presione Enter para guardar los cambios.</li>
+        <li>Para eliminar una tarea, haga click sobre el icono 'X'. En el caso de que la tarea no esté completa, se le solicitará una confirmación para poder eliminarla.</li>
         <li>Mediante los botones en la parte inferior puede filtrar que tipo de tareas quiere ver.</li>
         <li>Puede eliminar todas las tareas completas mediante el botón "Limpiar completados".</li>
       </ul>
